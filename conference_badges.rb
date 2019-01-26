16ab0730b2d24c447deb9b0088a4547c71759734
Kathleen Howard <kmaryhoward@gmail.com>
2019-01-26 21:29:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  arr.collect{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(arr)
+  arr.collect.with_index(1) {|name, index|
+  "Hello, #{name}! You'll be assigned to room #{index}!"
+  }
+end
+
+def printer(arr)
+  i = 0
+  badge =[]
+  room = []
+  badge << batch_badge_creator(arr)
+  room << assign_rooms(arr)
+  puts "#{badge[i].zip(room[i])}"
+end
